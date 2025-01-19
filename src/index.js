7f877c074bb3e2afd802c0a213fbaffc349589d3
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-19 15:21:00 +0200

    install and setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get("/", (req, res) => {
-    res.send("It works!");
+    res.render('home', {layout: false});
 });
 
 app.listen(3000, () => {
