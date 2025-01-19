6e3188d4c7d3e4f48173b4b27bc797bad34d3cc6
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-19 15:37:29 +0200

    show home page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-app.use(express.static('src/public'));
+app.use('/static',express.static('src/public'));
 
 app.get("/", (req, res) => {
     res.render('home', {layout: false});
