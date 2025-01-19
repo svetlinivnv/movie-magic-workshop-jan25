46789f21e6416fba5883db136f82a870b26edc71
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-19 16:42:17 +0200

    add dynamic movies rendering on home page

---
+++ src/movies.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Jungle Cuise',
+        genre: 'Adventure',
+        description: 'Description: Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
+        imageUrl: '/static/img/jungle-cruise.jpeg'
+    },
+    {
+        title: 'Man of Steel',
+        genre: 'Superhero',
+        description: 'An alien child is evacuated from his dying world and sent to Earth to live among humans. His peace is threatened when other survivors of his home planet invade Earth.',
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTk5ODk1NDkxMF5BMl5BanBnXkFtZTcwNTA5OTY0OQ@@._V1_FMjpg_UX1000_.jpg'
+    }
+];
