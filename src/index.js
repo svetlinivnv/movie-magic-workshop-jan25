6cee89ad49f946b7e14a5cbdea2d21c00116f800
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-19 15:51:12 +0200

    add create, search, 404 pages

--- src/index.js
+++ src/index.js
@@ -13,6 +13,22 @@ app.get("/", (req, res) => {
     res.render('home');
 });
 
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('*', (req, res) => {
+    res.render('404');
+})
+
 app.listen(3000, () => {
     console.log("Server is listening on http://localhost:3000...");
 });
