e32f70f11ebd37d7e2f8ec069b59d16aa81bc858
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-19 15:28:07 +0200

    setup express static

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+app.use(express.static('src/public'));
+
 app.get("/", (req, res) => {
     res.render('home', {layout: false});
 });
