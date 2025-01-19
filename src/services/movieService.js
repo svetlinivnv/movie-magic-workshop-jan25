5bb3cf7129d5444ea769de74cd9d14b26842689e
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-19 19:00:39 +0200

    add search by genre & year

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,6 +7,15 @@ export default {
         if (filter.search) {
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
         }
+
+        if (filter.genre) {
+            result = result.filter(movie => movie.genre.toLowerCase().includes(filter.genre.toLowerCase()));
+        }
+
+        if (filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
+        }
+        
         return result;
     },
   findOne(movieId) {
