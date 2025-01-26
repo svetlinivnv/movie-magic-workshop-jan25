cf64e1d9faf02573b5457cd850f47e396c23fb00
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-01-27 00:48:39 +0200

    exclude already attached casts

--- src/services/castService.js
+++ src/services/castService.js
@@ -1,8 +1,17 @@
 import Cast from "../models/Cast.js";
 
 export default {
-    getAll() {
-        return Cast.find({});
+    getAll(filter = {}) {
+        let query = Cast.find({});
+
+        if (filter.exclude) {
+            // MongoDB variant:
+            // query = query.find({_id: {$nin: filter.exclude}});
+            // Mongoose variant:
+            query = query.nin('_id', filter.exclude);
+        }
+
+        return query;
     },
     create(castData) {
         // TODO: Create cast
