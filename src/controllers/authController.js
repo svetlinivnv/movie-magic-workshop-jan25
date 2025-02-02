056bab5a285b0ab365488dc535d04eb37442efdb
Svetlin Ivanov <svetlinivnv@gmail.com>
2025-02-02 18:20:57 +0200

    add login functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,4 +19,15 @@ authController.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const token = await authService.login(email, password);
+    } catch(err) {
+        console.log(err.message);
+        return res.redirect('/404');
+    }
+    res.redirect('/');
+});
+
 export default authController;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,37 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const SECRET = '$3b$10$P1hE6BG31WMfIqIdg3tauoYHWf4sDSgrNg8NB3MXa15PcQLFB64F';
 
 export default {
     register(userData) {
         return User.create(userData);
+    },
+    async login(email, password) {
+        // check if user exists
+        const user = await User.findOne ({ email });
+        if (!user) {
+            throw new Error('Invalid email or password!');
+        }
+
+        // check if password is correct
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            throw new Error('Invalid email or password!');
+        }
+
+        // generate token
+        const payload = {
+            id: user.id,
+            email: user.email,
+        }
+
+        // TODO: use async option
+        const token = jwt.sign(payload, SECRET, {
+            expiresIn: '2h',
+        });
+
+        return token;
     }
 };
